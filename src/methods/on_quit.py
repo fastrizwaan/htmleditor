7d02d21ff4ab1ca3e14a2538229daaae4ee199dd
Asif Ali Rizvan <fast.rizwaan@gmail.com>
2025-04-09 06:37:41 +0530

    remove methods directory

--- src/methods/on_quit.py
+++
@@ -1,5 +0,0 @@
-def on_quit(self, action, param):
-    """Quit the application"""
-    for win in self.windows[:]:  # Use a copy to avoid modification during iteration
-        win.close()
-    self.quit()
